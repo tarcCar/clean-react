ebf5f2d996b85109194fd5226de9a87370d9c64a
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-10 21:47:52 -0300

    feat: ensure remoteauthentication return Account on success

--- src/domain/test/mock-authentication.ts
+++
@@ -1,8 +0,0 @@
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import faker from 'faker'
-export const mockAuthentication = (): AuthenticationParams => {
-  return {
-    email: faker.internet.email(),
-    password: faker.internet.password()
-  }
-}
