50a8191c9240ba293901237e3cec1021ccf91443
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-11 20:43:47 -0300

    feat: add remove autofill input on chrome

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -4,8 +4,12 @@ import Styles from './input-styles.scss';
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const enableInput = (event: React.FocusEvent<HTMLInputElement, Element>): void => {
+    event.target.readOnly = false
+  }
+
   return <div className={Styles.inputWrap}>
-    <input {...props}/>
+    <input {...props} onFocus={enableInput}/>
     <span className={Styles.status}>🔴</span>
   </div>
 }
