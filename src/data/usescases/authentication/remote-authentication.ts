944e8b01a123e0f690c321118eda329bbd1a73b2
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-10 21:34:47 -0300

    feat: ensure remoteauthentication throws unexpected error on defalut cases

--- src/data/usescases/authentication/remote-authentication.ts
+++ src/data/usescases/authentication/remote-authentication.ts
@@ -2,6 +2,7 @@ import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/InvalidCredentialsError'
+import { UnexpectedError } from '@/domain/errors/UnexpectedError'
 
 export class RemoteAuthentication {
   constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) {}
@@ -11,10 +12,12 @@ export class RemoteAuthentication {
       body: params
     })
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        break
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
-
-      default: return Promise.resolve()
+      default:
+        throw new UnexpectedError()
     }
   }
 }
