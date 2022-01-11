ae9eb24cafa91cc30cfbe38fa669b91f459f7604
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-11 06:56:36 -0300

    feat: ensure axioshttpclient call axios with correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -2,7 +2,7 @@ import { HttpPostParams } from '@/data/protocols/http'
 import axios from 'axios'
 export class AxiosHttpClient {
   async post (params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url)
+    await axios.post(params.url, params.body)
     return Promise.resolve()
   }
 }
