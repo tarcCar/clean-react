023cbbe126d841e1363418a14af9dc2e2571a3b2
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-11 06:49:44 -0300

    test: ensure axioshttpclient call axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -10,12 +10,12 @@ const makeSut = (): AxiosHttpClient => {
 }
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct Url', async () => {
+  test('Should call axios with correct Url and verb', async () => {
     const url = faker.internet.url()
     const sut = makeSut()
     await sut.post({
       url
     })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
