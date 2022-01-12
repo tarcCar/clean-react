792cc88b905700aad61e25a595d7200d1e2fbc54
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-11 22:59:11 -0300

    test: ensure login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -74,4 +74,22 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('Tudo certo!')
     expect(passwordStatus.textContent).toBe('🟢')
   })
+
+  test('Should show valid email state if validation succeeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const { getByTestId } = sut
+    const emailInput = getByTestId('email')
+
+    fireEvent.input(emailInput, {
+      target: {
+        value: faker.internet.email()
+      }
+    })
+
+    const emailStatus = getByTestId('email-status')
+
+    expect(emailStatus.title).toBe('Tudo certo!')
+    expect(emailStatus.textContent).toBe('🟢')
+  })
 })
