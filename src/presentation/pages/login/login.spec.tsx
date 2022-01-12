002a8fbe0ce4e908503de95a62370ad8706a1c5f
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-12 19:34:58 -0300

    test: prevent Login to submit form with erros

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -140,4 +140,14 @@ describe('Login Component', () => {
 
     expect(authenticationSpy.callsCount).toBe(1)
   })
+
+  test('Should call Authentication if form is invalid', () => {
+    const validationError = faker.random.words()
+    const { sut, authenticationSpy } = makeSut({ validationError })
+
+    populateEmailField(sut)
+    fireEvent.submit(sut.getByTestId('form'))
+
+    expect(authenticationSpy.callsCount).toBe(0)
+  })
 })
