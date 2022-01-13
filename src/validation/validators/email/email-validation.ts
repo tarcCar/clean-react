e7c8d73afc8a25f7a780cef962980e625aac775d
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-13 07:31:44 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,6 +4,7 @@ import { FieldValidation } from '@/validation/protocols/field-validation';
 export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
   validate (value: string): Error {
-    return new InvalidFieldError()
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/
+    return emailRegex.test(value) ? null : new InvalidFieldError()
   }
 }
