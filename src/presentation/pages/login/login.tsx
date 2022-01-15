b75109195ed3b6247ade48b0ab53c0fcfd0cbe14
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-15 17:27:41 -0300

    test: ensure login page begin with correct inicital state

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -28,7 +28,7 @@ const Login: React.FC<Props> = ({ validation, authentication , saveAccessToken }
     const { email, password } = state
     const formData = { email, password }
 
-    const emailError = validation.validate('name', formData)
+    const emailError = validation.validate('email', formData)
     const passwordError = validation.validate('password', formData)
     const isFormInvalid = !!(emailError || passwordError)
 
