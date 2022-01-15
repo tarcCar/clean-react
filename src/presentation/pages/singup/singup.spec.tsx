2b5945689b8c5a8926a848cf389060658142a1a3
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-15 09:40:26 -0300

    test: ensure SingUp change valid status if validation succeeds

--- src/presentation/pages/singup/singup.spec.tsx
+++ src/presentation/pages/singup/singup.spec.tsx
@@ -64,11 +64,40 @@ describe('Login Component', () => {
     Helper.populateField(sut, 'password')
     Helper.testStatusForField(sut, 'password', validationError)
   })
+
   test('Should show passwordConfirmation error if Validation fails', () => {
     const validationError = faker.random.words()
     const { sut } = makeSut({ validationError })
 
     Helper.populateField(sut, 'passwordConfirmation')
     Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
+  })
+
+  test('Should show valid name state if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut,'name')
+    Helper.testStatusForField(sut, 'name')
+  })
+
+  test('Should show valid email state if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut,'email')
+    Helper.testStatusForField(sut, 'email')
+  })
+
+  test('Should show valid password state if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut,'password')
+    Helper.testStatusForField(sut, 'password')
+  })
+
+  test('Should show valid passwordConfirmation state if validation succeeds', () => {
+    const { sut } = makeSut()
+
+    Helper.populateField(sut,'passwordConfirmation')
+    Helper.testStatusForField(sut, 'passwordConfirmation')
   })
 })
