669b484c5320f24791433db6a6f2c57ada5015be
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-15 12:06:36 -0300

    test: add Sinup validation factory

---
+++ src/main/factories/pages/singup/singup-validation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-build'
+import { makeSingUpValidation } from './singup-validation-factory'
+
+describe('SingUpValidationFactory', () => {
+  test('Should compose ValidationComposite with correct validations', () => {
+    const composite = makeSingUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build(),
+    ]))
+  })
+})
