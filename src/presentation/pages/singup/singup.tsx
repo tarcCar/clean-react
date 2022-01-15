0c373864654fb08475fbf2500a62be86b72fabc3
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-14 21:37:14 -0300

    feat: ensure SingUp starts with correct initial state

--- src/presentation/pages/singup/singup.tsx
+++ src/presentation/pages/singup/singup.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './singup-styles.scss';
 import { LoginHeader as Header, Footer, FormStatus, Input } from '@/presentation/components';
 import Context from '@/presentation/contexts/form/form-context'
 
-import { Link } from 'react-router-dom';
-
 const SingUp: React.FC = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+    nameError: 'Campo obrigatório',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+    mainError: ''
+  })
+
   return <div className={Styles.singup}>
     <Header />
-    <Context.Provider value={{ state: {} }} >
+    <Context.Provider value={{ state }} >
       <form className={Styles.form} >
         <h2>Criar Conta</h2>
         <Input type='text' name='name' placeholder='Digite seu nome'/>
         <Input type='email' name='email' placeholder='Digite seu e-mail'/>
         <Input type='password' name='password' placeholder='Digite sua senha'/>
-        <Input type='password' name='password' placeholder='Confirme sua senha'/>
-        <button className={Styles.submit} type='submit'>Salvar</button>
-        <Link to="/login" className={Styles.link}>Voltar para login</Link>
+        <Input type='password' name='passwordConfirmation' placeholder='Confirme sua senha'/>
+        <button data-testid="submit" disabled className={Styles.submit} type='submit'>Salvar</button>
+        <span className={Styles.link}>Voltar para login</span>
         <FormStatus />
       </form>
     </Context.Provider>
