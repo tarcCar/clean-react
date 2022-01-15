d2ff64e9b30e31d045f2462ce4fcf4eb7888c670
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-15 09:37:24 -0300

    test: ensure SingUp passwordConfirmation status to error if validation fails

--- src/presentation/pages/singup/singup.spec.tsx
+++ src/presentation/pages/singup/singup.spec.tsx
@@ -38,7 +38,7 @@ describe('Login Component', () => {
     Helper.testStatusForField(sut, 'name', validationError)
     Helper.testStatusForField(sut, 'email', validationError)
     Helper.testStatusForField(sut, 'password', validationError)
-    Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
+    Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
   })
 
   test('Should show name error if Validation fails', () => {
@@ -63,5 +63,12 @@ describe('Login Component', () => {
 
     Helper.populateField(sut, 'password')
     Helper.testStatusForField(sut, 'password', validationError)
+  })
+  test('Should show passwordConfirmation error if Validation fails', () => {
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
+
+    Helper.populateField(sut, 'passwordConfirmation')
+    Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
   })
 })
