67edc72dc52e73d0733ae3c7992639e69ff27d05
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-16 14:32:24 -0300

    refactor: add input animations

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,33 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import Styles from './input-styles.scss';
 import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const inputRef = useRef<HTMLInputElement>()
   const { state, setState } = useContext(Context)
   const error = state[`${props.name}Error`]
 
-  const enableInput = (event: React.FocusEvent<HTMLInputElement, Element>): void => {
-    event.target.readOnly = false
-  }
-
-  const handleChange = (event: React.FocusEvent<HTMLInputElement, Element>): void => {
-    setState({
-      ...state,
-      [event.target.name]: event.target.value
-    })
-  }
-
-  const getStatus = (): string => {
-    return error ? '🔴' : '🟢'
-  }
-  const getTitle = (): string => {
-    return error || 'Tudo certo!'
-  }
-
   return <div className={Styles.inputWrap}>
-    <input data-testid={props.name} {...props} readOnly onFocus={enableInput} onChange={handleChange}/>
-    <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
+    <input
+      ref={inputRef}
+      data-testid={props.name}
+      {...props}
+      placeholder=' '
+      readOnly
+      onFocus={(e) => { e.target.readOnly = false }}
+      onChange={(e) => { setState({ ...state, [e.target.name]: e.target.value }) }}
+    />
+    <label onClick={() => inputRef.current.focus()}>
+      {props.placeholder}
+    </label>
+    <span
+      data-testid={`${props.name}-status`}
+      title={ error || 'Tudo certo!'}
+      className={Styles.status}
+    >
+      {error ? '🔴' : '🟢'}
+    </span>
   </div>
 }
 
