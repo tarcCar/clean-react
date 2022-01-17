8204cbd1e416004d38e2befae54d9b419f0ae7cd
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-17 20:09:18 -0300

    refactor: adapt layout to mobile

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -3,7 +3,7 @@ import Logo from '../logo/logo';
 import Styles from './login-header-styles.scss';
 
 const LoginHeader: React.FC = () => {
-  return <header className={Styles.header}>
+  return <header className={Styles.headerWrap}>
     <Logo/>
     <h1>4Dev - Enquetes para Programadores</h1>
   </header>
