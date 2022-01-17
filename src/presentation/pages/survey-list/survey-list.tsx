c6b793af724895c69c2268581a8970c39d851ff7
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-17 20:27:56 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -20,6 +20,9 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver Resultado</footer>
           </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
         </ul>
       </div>
       <Footer />
