c10591133fedef3a46fd80ac2927ce9b8d03966a
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-16 21:30:21 -0300

    feat: ensure RemoteLoadSurveyList throw undexpect error on 403

--- src/data/usescases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usescases/load-survey-list/remote-load-survey-list.ts
@@ -1,9 +1,15 @@
-import { HttpGetClient } from '@/data/protocols/http';
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http';
+import { UnexpectedError } from '@/domain/errors';
 
 export class RemoteLoadSurveyList {
   constructor (private readonly url: string, private readonly httpGetClientSpy: HttpGetClient) {}
 
   async loadAll (): Promise<void> {
-    await this.httpGetClientSpy.get({ url: this.url })
+    const httpResponse = await this.httpGetClientSpy.get({ url: this.url })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:break
+      default:
+        throw new UnexpectedError()
+    }
   }
 }
