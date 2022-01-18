fef1392d9aeefa5ea0119e05467116644a2b0e81
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-17 22:14:40 -0300

    test: ensure currentAccountAdapter calls localstorage with correct values

---
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -0,0 +1,13 @@
+import { mockAccountModel } from '@/domain/test'
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
+import { setCurrentAccountAdapter } from './current-account-adapter'
+
+jest.mock('@/infra/cache/local-storage-adapter')
+describe('currentAccountAdapter', () => {
+  test('should call LocalStorageAdapter with correct values', () => {
+    const account = mockAccountModel()
+    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
+    setCurrentAccountAdapter(account)
+    expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+})
