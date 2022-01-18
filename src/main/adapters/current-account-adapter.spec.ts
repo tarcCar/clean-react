4dd244646bfc24c80e7da5180a7f7e2fc23e7dd5
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-17 22:17:00 -0300

    test: ensure CurrentAccountAdapt throws if invalid data is passed

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,3 +1,4 @@
+import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 import { setCurrentAccountAdapter } from './current-account-adapter'
@@ -9,5 +10,11 @@ describe('currentAccountAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('should throw UnexpectedError', () => {
+    expect(() => {
+      setCurrentAccountAdapter(undefined)
+    }).toThrow(new UnexpectedError())
   })
 })
