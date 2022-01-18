a57e6817fd97359b1f4e4ad1f3d2fc482943616e
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-18 20:43:39 -0300

    refactor: move sut to factory method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,21 +1,31 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { PrivateRoute } from './private-route.'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Route, Router, Routes } from 'react-router-dom'
+
+type SutTypes = {
+  history: MemoryHistory
+}
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({
+    initialEntries: ['/']
+  })
+  render(<Router location='/' navigator={history}>
+   <Routes>
+   <Route path='/' element={<PrivateRoute/>}>
+          <Route path='/' element={<div></div>}/>
+        </Route>
+   </Routes>
+  </Router>)
+  return {
+    history
+  }
+}
 
 describe('PrivateRoute', () => {
   test('Should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({
-      initialEntries: ['/']
-    })
-    render(<Router location='/' navigator={history}>
-     <Routes>
-     <Route path='/' element={<PrivateRoute/>}>
-            <Route path='/' element={<div></div>}/>
-          </Route>
-     </Routes>
-    </Router>)
+    const { history } = makeSut()
     expect(history.location.pathname).toBe('/login')
   })
 })
