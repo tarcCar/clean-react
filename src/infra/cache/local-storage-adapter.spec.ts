d1df382d65a2bfd5cca4f49ac84c8aa562f40196
Tarcisio Mulatti de Carvalho <tarcisio@viasoft.com.br>
2022-01-17 21:32:04 -0300

    refactor: remove async from localstorage save

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -8,11 +8,11 @@ describe('LocalStorageAdapter', () => {
   beforeEach(() => {
     localStorage.clear()
   })
-  test('Should call localStorage with correct values', async () => {
+  test('Should call localStorage with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.words()
-    await sut.set(key, value)
+    sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
   })
 })
